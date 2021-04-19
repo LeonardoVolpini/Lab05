2d6e2f984b374fcd08bad64ba3469f6dac1714b1
Leo Volpe <Leo Volpe@LAPTOP-VU9KPGVR.station>
2021-04-19 09:12:28 +0200

    finito

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
@@ -1,14 +1,31 @@
 package it.polito.tdp.anagrammi.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DBConnect {
 
+	static private final String jdbcURL= "jdbc:mysql://localhost/dizionario?user=root&password=Leonardo00";
+	static private HikariDataSource ds = null;
+	
 	public static Connection getConnection() throws SQLException {
-		String jdbcURL= "jdbc:mysql://localhost/dizionario?user=root&password=Leonardo00";
-		return DriverManager.getConnection(jdbcURL);
+		
+		if(ds==null) {
+			ds= new HikariDataSource();
+			ds.setJdbcUrl(jdbcURL);
+			//username e password li posso specificare o nella URL o con setUsername e setPassword
+			//ds.setUsername("root");
+			//ds.setPassword("Leonardo00");
+		}
+		try {
+			Connection connection= ds.getConnection();
+			return connection;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore DB",e);
+		}
 	}
 	
 }
